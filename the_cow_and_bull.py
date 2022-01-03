8f9b2c2f2c0bbbaa32d428a1344bfde72f3403d7
nehaprajapati123 <91823695+nehaprajapati123@users.noreply.github.com>
2022-01-03 12:22:09 +0530

    Update the_cow_and_bull.py

--- the_cow_and_bull.py
+++ the_cow_and_bull.py
@@ -10,6 +10,9 @@ def fun(list1):
     bull=0
     cow=0
     not1=0
+    bull1=[]
+    cow1=[]
+    not2=[]
     while j<len(list2):
         number=int(input("enter your number"))
         position=int(input("enter your number's position"))
@@ -21,10 +24,15 @@ def fun(list1):
         else:
             not1=not1+1
         j=j+1
-    print(bull,"bull")
+    bull1.append(bull)
+    print("bull:-",bull1)
     if bull==5:
         print("you are winner")
-    print(cow,"cow")
-    print(not1,"not a secret number")
+    cow1.append(cow)
+    print("cow:-",cow1)
+    not2.append(not1)
+    print("not exist:-",not2)
 
 fun([0,1,2,3,4,5,6,7,8,9])
+
+7
